8673f430e4f9b846055a1d1ac1d3d3459e677a04
Faiyaz Ashrafie <39721118+fr-ashrafie@users.noreply.github.com>
2026-05-02 01:44:39 +0600

    Delete backend directory

--- backend/scripts/init_pgvector.sql
+++
@@ -1,5 +0,0 @@
--- Enable pgvector extension
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Create IVFFlat index for face encodings (will be created after table exists)
--- This is run after migrations
